81f9dd5738698068ae648dba09e008f07a50a980
UPCSM SKILL <walasabkuch@gmail.com>
2021-07-08 17:48:55 +0530

    variables

---
+++ src/Applicaion.java
@@ -0,0 +1,10 @@
+public class Applicaion {
+
+
+    public static void main (String[] args) {
+
+        System.out.println("hello ");
+        System.out.println("hello  this is programing file");
+    }
+
+}
