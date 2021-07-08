bd94b1b9e84be4b27710cdfc612b2c779fffe4ef
UPCSM SKILL <walasabkuch@gmail.com>
2021-07-08 18:25:07 +0530

    for int

--- src/Applicaion.java
+++ src/Applicaion.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class Applicaion {
 
 
@@ -5,6 +7,27 @@ public class Applicaion {
 
         System.out.println("hello ");
         System.out.println("hello  this is programing file");
+
+
+        String name= "hina....K";
+        System.out.println(name);
+        String cityname = "mumbai";
+        System.out.println(cityname);
+        String address= "line par tundla";
+        System.out.println(address);
+        String age = "12";
+        System.out.println(age);
+        int price= 15;
+        System.out.println(price);
+        String oli="onion";
+        System.out.println(oli);
+        int marks =87;
+        System.out.println(name   +     marks);
+
+
+
+
     }
 
 }
+
