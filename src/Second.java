07fe7bc84525a897ae075f28e38a50dc5ad69e15
UPCSM SKILL <walasabkuch@gmail.com>
2021-07-09 15:46:15 +0530

    for conditional statement if and else

--- src/Second.java
+++ src/Second.java
@@ -1,6 +1,48 @@
-public class Second {
+import javax.swing.*;
 
-    public static void main(String[] args){
+public class Second {
+    public static void main(String[] args) {
+        /* SYNTAX: IF (conditions){
+                          code
+                              }
+                        else
+                        {
+                        code
+                        } */
+        int age=24;
+           if (age>18) {
+               System.out.println("person is young");}
+
+           else {
+               System.out.println("person is kid");
+        }
+           String city="PANJAB";
+           if (city=="AGRA")
+        {System.out.println("you are in agra");}
+        else{
+            System.out.println("you are not in agra,you are in another cty");
+        }
+        double price= 523.3;
+        if (price<500){
+            System.out.println("price is lower than 500...");}
+        else {System.out.println("price is greater than 500");}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
